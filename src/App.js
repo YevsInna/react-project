a02e395232e89af7f03299205dc0d57c342026f2
YevsInna <evseinka@gmail.com>
2021-06-30 13:27:30 +0300

    дз Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Link, Switch} from 'react-router-dom';
+import Users from './components/users/Users';
+import Posts from './components/posts/Posts';
+import Comments from './components/comments/Comments';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+
+        <Router>
+
+            <div>
+                <Link to={'/users'}>to users page</Link>
+                <br/>
+                <Link to={'/posts'}>to posts page</Link>
+                <br/>
+                <Link to={'/comments'}>to comments page</Link>
+                <br/>
+
+                <Switch>
+                    <Router path = {'/users'} component = {Users}/>;
+                    <Router path = {'/posts'} component = {Posts}/>;
+                    <Router path = {'/comments'} component = {Comments}/>;
+
+                </Switch>
+
+
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/users/Users.js
@@ -0,0 +1,20 @@
+import {useEffect, useState} from "react";
+import User from "../user/User"
+
+export default function Users(props) {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(usersFromServer => {
+                setUsers(usersFromServer);
+            });
+    }, [])
+
+
+    return (
+        <div>
+            users.map(users => <User item={users}/>)
+        </div>
+    );
+}
