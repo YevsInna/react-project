964e24b9719b53e4b586988e82e6343ba2f70515
YevsInna <evseinka@gmail.com>
2021-07-11 12:58:23 +0300

    десь помилка в запиті

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import {useSelector} from "react-redux";
+import {useState} from "react";
+
+const CreateTodoForm = ({onSubmit}) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const submit = async (e) => {
+        e.preventDefault();
+        if (!title || !description || loading) return;
+
+        try {
+            setLoading(true)
+            await onSubmit(title, description)
+            setTitle('')
+            setDescription('')
+        } catch (e) {
+            console.log(e)
+        } finally{
+                setLoading(false)
+            }
+            }
+    return (
+        <form onSubmit={submit}>
+            <input
+                type="text"
+                placeholder='title'
+                value={title}
+                onChange={({target: {value}}) => setTitle(value)}/>
+            <br/>
+            <br/>
+            <input
+                type="text"
+                placeholder='description'
+                value={description}
+                onChange={({target: {value}}) => setDescription(value)}/>
+            <br/>
+            <br/>
+            <button type='submit' disabled={!title || !description || loading}>create todo</button>
+        </form>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const store = useSelector(state => state)
+    console.log(store)
+
+    const onTodoCreate = async (title, description) => {
+        if (!title || !description) return;
+
+        const response = await fetch('http://localhost:8888/create-todo', {
+            method: 'POST',
+            body: JSON.stringify({title, description}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json();
+        console.log(data)
+    }
+    return (
+        <div>
+            <CreateTodoForm onSubmit={onTodoCreate}/>
+        </div>
+    );
 }
 
 export default App;
